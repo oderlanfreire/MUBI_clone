88ce5e8250b3f6ad612b70bb2e10b7de7e34a70d
Oderlan Freire <56801164+oderlanfreire@users.noreply.github.com>
2022-02-28 21:10:52 -0300

    Pratica de clone da MUBI concluido

---
+++ js/owl/main.js
@@ -0,0 +1,18 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    items: 3,
+    margin:8,
+    nav:false,
+    autoplay:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
